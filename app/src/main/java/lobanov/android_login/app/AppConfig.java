dac1258dfa6b707b461319db3567cce6101d3816
lobanov1994 <m.lobanov1994@yandex.ru>
2016-05-27 20:42:39 +0300

    first commit

---
+++ app/src/main/java/lobanov/android_login/app/AppConfig.java
@@ -0,0 +1,13 @@
+package lobanov.android_login.app;
+
+/**
+ * Created by student on 14.05.2016.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://php-lobanov.rhcloud.com/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://php-lobanov.rhcloud.com/register.php";
+
+}
